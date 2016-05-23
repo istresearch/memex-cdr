35d876179c197ef4e53dc95c4f2247f0bea38ef7
Brandon Rose <rose.brandon.m@gmail.com>
2016-05-23 17:42:15 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# memex-cdr
-This repository hosts code and schema information related to the Memex Crawl Data Repository (CDR)
+# CDR Validation Script
+The `cdr_validation.py` script processes a set of JSON lines which have been `gzip`d. It expects that each line within the `gzip`d file contains one CDR formatted object as a string so that it can load it with `json.loads()`. 
+
+## Crawl Documents
+For crawl pages, it checks that each object contains the following keys:
+
+* `_id`
+* `timestamp`,
+* `content_type`
+* `crawler`,
+* `extracted_metadata`
+* `extracted_text`
+* `raw_content`
+* `team`
+* `url`
+* `version`
+
+which are defined as required fields per the [CDR Schema wiki page](https://memexproxy.com/wiki/display/MPM/CDR+Schema).
+
+## Media Documents
+For media documents, it checks that each object's `obj_parent` exists within the given dataset. For example, if the media's `obj_parent` is `A12DVKD12478Z` then `A12DVKD12478Z` must exist as the `_id` within a crawl document contained within the same dataset. Additionally, it verifies that each object contains the following keys:
+
+* `_id`
+* `timestamp`
+* `content_type`
+* `obj_original_url`
+* `obj_parent`
+* `obj_stored_url`
+* `team`
+* `version`
+
+which are defined as required fields per the [CDR Schema wiki page](https://memexproxy.com/wiki/display/MPM/CDR+Schema).
+
+## Executing `cdr_validation.py`
+This script requires Python 2.7. To execute it you must provide the path to the input file (`input_file`) and the desired path of the output (`result_file`).
+
+```
+python cdr_validation.py --input_file=input.gz --result_file=output
+```
+
+The script returns the number of documents that passed, the number that failed, and the time to execute the script. For example:
+
+```
+1006 documents passed.
+994 documents failed.
+Took 0:00:02.337122
+```
+
+## Interpreting the `result_file`
+The script also writes an output file which is a CSV where the first column is the `_id`, the second column is either `Passed` or `Failed` and if `Failed` there is a third column providing a rationale which is either:
+
+1. `Missing parent document`
+2. `Missing required field`
---
+++ cdr_validation.py
@@ -0,0 +1,98 @@
+import json
+import argparse
+import gzip as gz
+import datetime
+
+def is_media(doc):
+    ''' Checks whether item is media '''
+    if 'obj_parent' in doc.keys():
+        return True
+    else:
+        return False
+
+def test_media(doc, _ids, media_fields):
+    ''' Checks media fields exist as keys in doc '''    
+    parent = doc.get('obj_parent')
+    if parent not in _ids:
+        return (False, "Missing parent document")
+    else:
+        if all(k in doc for k in media_fields):
+            return (True, "Passed")
+        else:
+            return (False, "Missing required field")
+
+def test_crawl(doc, crawl_fields):
+    ''' Checks crawl fields exist as keys in doc '''        
+    if all(k in doc for k in crawl_fields):
+        return (True, "Passed")
+    else:
+        return (False, "Missing required field")
+
+media_fields = ['_id','timestamp','content_type','obj_original_url','obj_parent','obj_stored_url','team','version']
+crawl_fields = ['_id','timestamp','content_type','crawler','extracted_metadata','extracted_text','raw_content','team','url','version']
+
+if __name__ == '__main__':
+
+    desc='CDR Validation'
+    parser = argparse.ArgumentParser(
+        description=desc,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=desc)
+
+    parser.add_argument("--input_file", help="path to the gzip file for testing")    
+    parser.add_argument("--result_file", help="path to the gzip file for testing")    
+    
+    args = parser.parse_args()
+
+    # parsed argument for input/result file
+    input_file = args.input_file
+    result_file = args.result_file
+
+    # generate input _ids dictionary
+    _ids = {}
+
+    # capture start time
+    start = datetime.datetime.now()
+
+    # iterate over input file to generate dictionary of _id's
+    with gz.open(input_file,'r') as fp:
+        for line in fp:
+            _id = json.loads(line).get('_id')
+            _ids[_id] = ''
+        fp.close()
+    
+    # using _ids dictionary, iterate over input and test fields
+    with gz.open(input_file,'r') as fp:
+        for line in fp:
+            doc = json.loads(line)
+            _id = doc.get('_id')
+            if is_media(doc):
+                result = test_media(doc, _ids, media_fields)
+            else:
+                result = test_crawl(doc, crawl_fields)
+            _ids[_id] = result
+    fp.close()
+    
+    passed = 0
+    failed = 0
+    
+    # write output file
+    with open(result_file, 'a') as fp:
+        for item in _ids.iteritems():
+            if item[1][0]:
+                res = 'Passed'
+                fp.write(str(item[0]) + ',' + res + '\n')
+                passed += 1
+            else:
+                res = 'Failed'
+                reason = item[1][1]
+                fp.write(str(item[0]) + ',' + res + ',' + reason + '\n')
+                failed +=1
+        fp.close()
+
+    end = datetime.datetime.now()
+    total_time = end - start
+    
+    print str(passed) + ' documents passed.'
+    print str(failed) + ' documents failed.'
+    print 'Took ' + str(total_time)
